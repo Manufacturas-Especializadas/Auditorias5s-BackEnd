cc79312da3e39a973b2bd97acccc87d9991ef951
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-07-15 07:33:29 -0600

    Nuevos DTOs

---
+++ Auditorias/Dtos/AnswerRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Auditorias.Dtos
+{
+    public class AnswerRequest
+    {
+        [Range(1, 22)]
+        public int IdQuestion { get; set; }
+
+        [Range(1, 5)]
+        public int score { get; set; }
+    }
+}
---
+++ Auditorias/Dtos/AuditExportDto.cs
@@ -0,0 +1,10 @@
+﻿using Auditorias.Models;
+
+namespace Auditorias.Dtos
+{
+    public class AuditExportDto
+    {
+        public Audits Audit { get; set; }
+        public List<Answers> Answers { get; set; }
+    }
+}
